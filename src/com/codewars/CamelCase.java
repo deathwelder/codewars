0acf7935ffa3633e3819154264da9f901c7c8b0f
Kostia Bulanovich <kostia.yodiz@gmail.com>
2020-12-03 01:40:21 +0400

    CamelCase

---
+++ src/com/codewars/CamelCase.java
@@ -0,0 +1,18 @@
+package com.codewars;
+
+public class CamelCase {
+
+    static String toCamelCase(String s) {
+        StringBuilder sbd = new StringBuilder(s);
+        for (int i = 0; i < sbd.length(); i++) {
+            if (sbd.charAt(i) == '-' || sbd.charAt(i) == '_') {
+                sbd.deleteCharAt(i);
+                sbd.replace(i+1, i+1, String.valueOf(sbd.charAt(i)).toUpperCase());
+                sbd.deleteCharAt(i);
+            }
+        }
+        String sbString = sbd.toString();
+
+        return sbString;
+    }
+}
